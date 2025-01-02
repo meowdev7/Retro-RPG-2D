2ca2afb8da9a9719a744af684de65b4378e49284
Meow Dev 7 <165454821+meowdev7@users.noreply.github.com>
2025-01-02 20:59:42 +0530

    Player update

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,14 +2,41 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $animatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	var direction = Vector2(
 		Input.get_axis("left", "right"), 
 		Input.get_axis("up", "down")
 	)
+	
 	if direction != Vector2.ZERO:
 		velocity = direction.normalized() * SPEED
+		_update_animation(direction)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
-	
+		_set_idle_animation()
+
 	move_and_slide()
+
+func _update_animation(direction: Vector2) -> void:
+	if abs(direction.x) > abs(direction.y):
+		animated_sprite.animation = "walk_sideways"
+		animated_sprite.flip_h = direction.x < 0
+	else:
+		if direction.y > 0:
+			animated_sprite.animation = "walk_towards"
+		else:
+			animated_sprite.animation = "walk_away"
+	
+	animated_sprite.play()
+
+func _set_idle_animation() -> void:
+	if animated_sprite.animation == "walk_sideways":
+		animated_sprite.animation = "idle_side"
+	elif animated_sprite.animation == "walk_towards":
+		animated_sprite.animation = "idle_front"
+	elif animated_sprite.animation == "walk_away":
+		animated_sprite.animation = "idle_back"
+	
+	animated_sprite.play()
